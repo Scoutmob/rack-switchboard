3302296d61b6c100b07063b0328b20a7aa7f300e
Calvin Yu <calvin@scoutmob.com>
2013-09-06 20:22:29 -0400

    More robust handling

--- lib/rack/switchboard.rb
+++ lib/rack/switchboard.rb
@@ -15,9 +15,13 @@ module Rack
     def call(env)
       result = @app.call(env)
       if result[0].to_i == 404
-        rewrite_result = create_rewrite.call(env)
-        if rewrite_result != 404
-          result = rewrite_result
+        begin
+          rewrite_result = create_rewrite.call(env)
+          if rewrite_result != 404
+            result = rewrite_result
+          end
+        rescue Exception => e
+          process_error(e, env)
         end
       end
       result
@@ -32,6 +36,27 @@ module Rack
             rules << rule
           end
         end
+      end
+
+      def process_error(error, env)
+        errors = env['rack.errors']
+        log_exception(error, errors)
+        if @options[:error_handler]
+          begin
+            @options[:error_handler].call(error)
+          rescue Exception => e
+            errors.write "!! Error in error handler\n"
+            log_exception(e, errors)
+          end
+        end
+        errors.flush
+      end
+
+      def log_exception(error, error_stream)
+        error_stream.write error.message +
+          "\n   " +
+          error.backtrace.join("\n   ") +
+          "\n"
       end
 
   end
